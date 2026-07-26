cfe779667e60bd40e8e0913edf049423be648cc6
DiveshK007 <diveshkumar.s007@gmail.com>
2026-07-26 22:18:07 +0530

    chore: ignore build artifacts

--- contracts/workflow_registry/target/release/build/thiserror-20965187c5e1305c/out/thiserror.d
+++
@@ -1,7 +0,0 @@
-/Users/bond/StellrFlow/contracts/workflow_registry/target/release/build/thiserror-20965187c5e1305c/out/thiserror.d: build/probe.rs
-
-/Users/bond/StellrFlow/contracts/workflow_registry/target/release/build/thiserror-20965187c5e1305c/out/libthiserror.rmeta: build/probe.rs
-
-build/probe.rs:
-
-# env-dep:RUSTC_BOOTSTRAP
